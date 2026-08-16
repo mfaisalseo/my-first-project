9ca628f44bdb77b304712a28e4599604e1ae92bf
faisal <mfaisalseo313@gmail.com>
2026-08-17 01:03:57 +0500

    Added Python calculator

--- hello.py
+++ hello.py
@@ -4,3 +4,15 @@ print("Hello GitHub!")
 print("My first project using VS Code")
 print("I am learning Python")
 print("This is my first GitHub update")
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+print("Addition:", number1 + number2)
+print("Subtraction:", number1 - number2)
+print("Multiplication:", number1 * number2)
+
+if number2 != 0:
+    print("Division:", number1 / number2)
+else:
+    print("Division: Cannot divide by zero")
